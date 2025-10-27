9f4886774a27a43eb2354d7a4af3ce3ac254b652
Felipe Quilumbango <fipojaq2005@gmail.com>
2025-10-27 10:33:05 -0500

    Validation enhance and Puzzles in comments

--- Lab5/sudoku.pl
+++ Lab5/sudoku.pl
@@ -42,9 +42,59 @@ print_sudoku([Row|Rows]) :- %Recursive Case: Process row by row
 %Validation that the sudoku is 9x9
 validate_sudoku(Rows) :-
     length(Rows, 9), %Columns = 9?
-    maplist(length_9, Rows). % Rows = 9?
+    maplist(validate_row, Rows). % Rows = 9?
 
-length_9(List) :- length(List, 9).
+validate_row(Row) :- 
+    length(Row, 9),
+    maplist(valid_cell, Row).
 
+valid_cell(X) :- var(X), !.
+valid_cell(X) :- integer(X), X >= 1, X =< 9.
 
 %Puzzles Tested
+% Puzzle1 = [
+%     [5,3,_,_,7,_,_,_,_],
+%     [6,_,_,1,9,5,_,_,_],
+%     [_,9,8,_,_,_,_,6,_],
+%     [8,_,_,_,6,_,_,_,3],
+%     [4,_,_,8,_,3,_,_,1],
+%     [7,_,_,_,2,_,_,_,6],
+%     [_,6,_,_,_,_,2,8,_],
+%     [_,_,_,4,1,9,_,_,5],
+%     [_,_,_,_,8,_,_,7,9]
+% ], validate_sudoku(Puzzle1),sudoku(Puzzle1), print_sudoku(Puzzle1).
+
+% Puzzle2 = [
+%     [_,_,_,2,6,_,7,_,1],
+%     [6,8,_,_,7,_,_,9,_],
+%     [1,9,_,_,_,4,5,_,_],
+%     [8,2,_,1,_,_,_,4,_],
+%     [_,_,4,6,_,2,9,_,_],
+%     [_,5,_,_,_,3,_,2,8],
+%     [_,_,9,3,_,_,_,7,4],
+%     [_,4,_,_,5,_,_,3,6],
+%     [7,_,3,_,1,8,_,_,_]
+% ], validate_sudoku(Puzzle2), sudoku(Puzzle2), print_sudoku(Puzzle2).
+
+% Puzzle3 = [
+%     [_,_,_,_,_,_,_,_,_],
+%     [_,_,_,_,_,3,_,8,5],
+%     [_,_,1,_,2,_,_,_,_],
+%     [_,_,_,5,_,7,_,_,_],
+%     [_,_,4,_,_,_,1,_,_],
+%     [_,9,_,_,_,_,_,_,_],
+%     [5,_,_,_,_,_,_,7,3],
+%     [_,_,2,_,1,_,_,_,_],
+%     [_,_,_,_,4,_,_,_,9]
+% ], validate_sudoku(Puzzle3), sudoku(Puzzle3), print_sudoku(Puzzle3).
+
+% Puzzle4 = [
+%     [_,_,_,_,_,_,_,_,_],
+%     [_,_,_,_,_,3,_,8,5],
+%     [_,_,1,_,2,_,_,_,_],
+%     [_,_,_,5,_,7,_,_,_],
+%     [_,_,4,_,_,_,1,_,_],
+%     [5,_,_,_,_,_,_,7,3],
+%     [_,_,2,_,1,_,_,_,_],
+%     [_,_,_,_,4,_,_,_,9]
+% ], validate_sudoku(Puzzle4), sudoku(Puzzle4), print_sudoku(Puzzle4).
